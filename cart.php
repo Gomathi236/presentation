f55d95c59f8b6c3b983b18aa6292b2e0dc0d3a87
Gomathi236 <gomathielumalai236@gmail.com>
2022-11-21 17:03:46 +0530

    changed

--- cart.php
+++ cart.php
@@ -16,9 +16,23 @@ class Cart
         $this->items = $items;
     }
     
-    
+    public function addProduct( $product, $quantity)
+    {
+        // find product in cart
+        $cartItem = $this->findCartItem($product->getId());
+        if ($cartItem === null){
+            $cartItem = new CartItem($product, 0);
+            $this->items[$product->getId()] = $cartItem;
+        }
+        $cartItem->increaseQuantity($quantity);
+        return $cartItem;
+    }
 
-  
+    private function findCartItem($productId)
+    {
+        return $this->items[$productId] ?? null;
+    }
+
     public function removeProduct( $product)
     {
         unset($this->items[$product->getId()]);
--- product.php
+++ product.php
@@ -7,43 +7,13 @@ class Product
     private  $title;
     private  $price;
     private  $availableQuantity;
-    private  $product;
-    private  $quantity;
 
-
-    public function __construct($id=null, $title=null, $price=null, $availableQuantity=null, $product=null, $quantity=null)
+    public function __construct($id, $title, $price, $availableQuantity)
     {
         $this->id = $id;
         $this->title = $title;
         $this->price = $price;
         $this->availableQuantity = $availableQuantity;
-        $this->product = $product;
-        $this->quantity = $quantity;
-    }
-    
-   
-   
-
-   
-    public function getProduct()
-    {
-        return $this->product;
-    }
-
-   
-    public function setProduct($product)
-    {
-        $this->product = $product;
-    }
-
-    
-    public function getQuantity()
-    {
-        return $this->quantity;
-    }
-    public function setQuantity($quantity)
-    {
-        $this->quantity = $quantity;
     }
 
     public function getId()
@@ -93,35 +63,9 @@ class Product
         $this->availableQuantity = $availableQuantity;
     }
 
-    public function addProduct( $product, $quantity)
+    public function addToCart( $cart,  $quantity)
     {
-        // find product in cart
-        $cartItem = $this->findCartItem($product->getId());
-        if ($cartItem === null){
-            $cartItem = new Product($product, 0);
-            $this->items[$product->getId()] = $cartItem;
-        }
-        return $cartItem;
-    } 
-
-     private function findCartItem($productId)
-    {
-        return $this->items[$productId] ?? null;
-    }
-
-    public function increaseQuantity($amount = 1)
-    {
-        if ($this->getQuantity() + $amount > $this->getProduct()){
-            throw new Exception("Product quantity can not be more than ".$this->getProduct());
-        } 
-       $this->quantity += $amount; 
-    }
-    public function decreaseQuantity($amount = 1)
-    {
-        if ($this->getQuantity() - $amount < 1){
-            throw new Exception("Product quantity can not be less than 1");
-        }
-        $this->quantity -= $amount;
+        return $cart->addProduct($this, $quantity);
     }
 
 
